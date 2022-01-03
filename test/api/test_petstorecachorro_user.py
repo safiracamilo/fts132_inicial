56a84f66de505a1b566afc902e715dbce3f5bb2a
SAFIRA <safiracamilo@gmail.com>
2022-01-03 19:15:40 -0300

    criação de uma função para criar usuario de um cachoor por meio json (utlizando site petstore) Após isso, criação de função para testar consulta de inclusão de usuário para chachorro

---
+++ test/api/test_petstorecachorro_user.py
@@ -0,0 +1,64 @@
+# Bibliotecas
+import pytest  # Framework de Teste Unitário - Engine
+import requests  # Framework de Teste API - Requests / Responses
+
+# Endereço da API
+base_url = 'https://petstore.swagger.io/v2/user'
+headers = {'Content-Type': 'application/json'}  # Nos .asmx seria 'text/xml'
+
+def testar_criar_usuario():
+    # Configura
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = "unknown"
+    mensagem_esperada = "10"
+
+ # Executa
+    resposta = requests.post(  # faz a requisição, passando:
+        url=base_url,  # O endpoint da API
+
+        data=open('C:/Users/SAFIRA/PycharmProjects/fts132_inicial/test/db/usercachorro.json', 'rb'),  # O body JSON
+        headers=headers  # O header com Content-Type
+    )
+# Formatação
+    corpo_da_resposta = resposta.json()  # Formata com JSON
+    print(resposta)  # Resposta Bruta
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Resposta Formatada
+
+ # Valida.
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
+
+def testar_consultar_usuario():
+    # Configura
+    status_code = 200
+    id = 10
+    username = 'Amora'
+    firstName = 'Amora'
+    lastName = 'AmoraLevada'
+    email = 'amora@gmail.com'
+    password = '1234'
+    phone = '31999999'
+    userStatus = 0
+
+# executa
+    resposta = requests.get(
+        url=f'{base_url}/{username}',
+        headers=headers
+    )
+ # Formatação
+    corpo_da_resposta = resposta.json()  # Formata com JSON
+    print(resposta)  # Resposta Bruta
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Resposta Formatada
+
+ # Valida
+    assert resposta.status_code == status_code
+    assert corpo_da_resposta['id'] == id
+    assert corpo_da_resposta['username'] == username
+    assert corpo_da_resposta['email'] == email
+    assert corpo_da_resposta['phone'] == phone
+
