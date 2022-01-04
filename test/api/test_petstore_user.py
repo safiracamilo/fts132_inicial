4b61b5f614ebb0430bcbc33e409915ec089d543b
SAFIRA <safiracamilo@gmail.com>
2022-01-04 13:30:20 -0300

    criação de uma função para excluir usuário

--- test/api/test_petstore_user.py
+++ test/api/test_petstore_user.py
@@ -6,6 +6,7 @@ import requests  # Framework de Teste API - Requests / Responses
 base_url = 'https://petstore.swagger.io/v2/user'
 headers = {'Content-Type': 'application/json'}  # Nos .asmx seria 'text/xml'
 
+
 def testar_criar_usuario():
     # Configura
     status_code_esperado = 200
@@ -62,3 +63,59 @@ def testar_consultar_usuario():
     assert corpo_da_resposta['username'] == username
     assert corpo_da_resposta['email'] == email
     assert corpo_da_resposta['phone'] == phone
+
+
+def testar_alterar_usuario():
+    # Configura
+    username = 'estrela'
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = "unknown"
+    mensagem_esperada = '1212'
+
+    # Executa
+    resposta = requests.put(
+        url=f'{base_url}/{username}',
+        data=open('C:/Users/SAFIRA/PycharmProjects/fts132_inicial/test/db/user2.json', 'rb'),
+        headers=headers
+
+    )
+
+    # Formatação
+    corpo_da_resposta = resposta.json()  # Formata com JSON
+    print(resposta)  # Resposta Bruta
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Resposta Formatada
+
+    # Valida
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
+
+
+def testar_excluir_usuario():
+    # Configura
+    username = 'estrela'  # informação de entrada- olhar o nome do arquivo no site no caso seria petstore
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = "unknown"
+    mensagem_esperada = 'estrela'
+
+    # Executa
+    resposta = requests.delete(  # agora coloca delete para deletar o nome
+        url=f'{base_url}/{username}',
+        headers=headers
+    )
+
+    # Formatação
+    corpo_da_resposta = resposta.json()  # Formata com JSON
+    print(resposta)  # Resposta Bruta
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Resposta Formatada
+
+    # Valida
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
